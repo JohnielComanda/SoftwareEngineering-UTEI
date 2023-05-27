0f634d7daf2e97a37ef6ec549e3101f0afa16c24
Johniel Comanda <johniel.comanda@cit.edu>
2023-05-27 23:26:19 +0800

    Add empty EfficiencyTestService interface

--- utei-backend/UTEI/Service/EfficiencyTestService.cs
+++ utei-backend/UTEI/Service/EfficiencyTestService.cs
@@ -1,6 +1,27 @@
-﻿namespace UTEI.Service
+﻿using UTEI.Dtos;
+using UTEI.GPTManager;
+using UTEI.Models;
+
+namespace UTEI.Service
 {
-    public class EfficiencyTestService
+    public class EfficiencyTestService : IEfficiencyTestService
     {
+        private readonly IAnalyzer _analyzer;
+        public EfficiencyTestService(IAnalyzer analyzer)
+        {
+            _analyzer = analyzer;
+        }
+
+        public async Task<string> CreateTest(EfficiencyTestCreationDto testEfficiency)
+        {
+            var temp = await _analyzer.Analyze(testEfficiency.ProgrammingLanguage, testEfficiency.UnitTest);
+            Console.WriteLine(temp);
+            return temp;
+        }
+
+        public Task<EfficiencyTest> GetSavedTest(string id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- utei-backend/UTEI/Service/IEfficiencyTestService.cs
+++ utei-backend/UTEI/Service/IEfficiencyTestService.cs
@@ -1,6 +1,11 @@
-﻿namespace UTEI.Service
+﻿using UTEI.Dtos;
+using UTEI.Models;
+
+namespace UTEI.Service
 {
     public interface IEfficiencyTestService
     {
+        Task<string> CreateTest(EfficiencyTestCreationDto testEfficiency);
+        Task<EfficiencyTest> GetSavedTest(string id);
     }
 }
