ae204c29a172714eb65732fca0be87c0df6084f3
Johniel Comanda <johniel.comanda@cit.edu>
2023-05-27 23:25:00 +0800

    Add EfficiencyTestEnhancer interface

---
+++ utei-backend/UTEI/GPTManager/EfficiencyTestEnhancer.cs
@@ -0,0 +1,17 @@
+﻿using UTEI.Models;
+
+namespace UTEI.GPTManager
+{
+    public class EfficiencyTestEnhancer : IEfficiencyTestEnhancer
+    {
+        public Task<string> Enhancer(string progLang, string unitTest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> SuggestionGenerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
