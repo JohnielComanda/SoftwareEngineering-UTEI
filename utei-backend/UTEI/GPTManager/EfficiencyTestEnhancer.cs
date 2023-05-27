3e4bea3b4542bac9944a1e253082411a4fb9b4a1
Johniel Comanda <johniel.comanda@cit.edu>
2023-05-27 23:31:49 +0800

    Add implementation for EfficiencyEnhancer

--- utei-backend/UTEI/GPTManager/EfficiencyTestEnhancer.cs
+++ utei-backend/UTEI/GPTManager/EfficiencyTestEnhancer.cs
@@ -1,12 +1,62 @@
-﻿using UTEI.Models;
+﻿using System.Text.Json;
+using UTEI.Models;
 
 namespace UTEI.GPTManager
 {
     public class EfficiencyTestEnhancer : IEfficiencyTestEnhancer
     {
-        public Task<string> Enhancer(string progLang, string unitTest)
+        public async Task<string> Enhancer(string progLang, string unitTest)
         {
-            throw new NotImplementedException();
+            var requestPayload = new
+            {
+                prompt = $"Write an improved version of the following unit test method:\n\n```{progLang}\n{unitTest}\n```",
+                temperature = 0.5,
+                max_tokens = 256,
+                top_p = 1.0,
+                frequency_penalty = 0.0,
+                presence_penalty = 0.0
+            };
+
+            using (var httpClient = new HttpClient())
+            {
+                // Set the OpenAI API endpoint and your API key
+                var apiUrl = "https://api.openai.com/v1/engines/text-davinci-003/completions";
+                httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer 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");
+
+                try
+                {
+                    var response = await httpClient.PostAsJsonAsync(apiUrl, requestPayload);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var responseContent = await response.Content.ReadFromJsonAsync<OpenAIResponse>();
+
+                        if (responseContent != null && responseContent.choices.Length > 0)
+                        {
+                            var improvedUnitTest = responseContent.choices[0].Trim();
+                            Console.WriteLine("Huhuness");
+                            return improvedUnitTest;
+                        }
+                        else
+                        {
+                            return "No response content or choices found.";
+                        }
+                    }
+                    else
+                    {
+                        var errorMessage = await response.Content.ReadAsStringAsync();
+                        return errorMessage;
+                    }
+                }
+                catch (JsonException ex)
+                {
+                    return $"JSON deserialization error: {ex.Message}";
+                }
+                catch (Exception ex)
+                {
+                    return $"An error occurred: {ex.Message}";
+                }
+            }
         }
 
         public Task<string> SuggestionGenerator()
